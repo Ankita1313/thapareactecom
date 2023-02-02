4f5dd2056b0a5e9aea6f5d5762b9a08e53def7c9
Ankita1313 <ankita13.oceanmtech@gmail.com>
2023-02-02 13:33:20 +0530

    b

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { BrowserRouter, Routes, route } from "react-router-dom";
+import Home from "./Home";
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
